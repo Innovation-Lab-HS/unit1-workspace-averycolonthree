00db783f398eec4a5dd5201fd030eb4e89e873ed
averycolonthree <1103714@apps.nsd.org>
2024-09-13 18:19:59 +0000

    hi

---
+++ src/main/java/edu/bhscs/HelloWorld copy.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+  // all code is in a class. like little students
+  public static void main(String[] args) {
+    int x = 1;
+    int y = 2;
+    // all code must also live in a public main method
+    // everything we write atm will live in main() dont even worry about it
+    System.out.println(x+y);
+  }
+}
+  // had a tool for jsons that would check basic formatting errors like missing semicolons and such;
+  // wonder if theres anything similar for java?
---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+  // all code is in a class. like little students
+
+  public static void main(String[] args) {
+    // all code must also live in a public main method
+    // everything we write atm will live in main() dont even worry about it
+    System.out.println("hello :)");
+  }
+}
+  // had a tool for jsons that would check basic formatting errors like missing semicolons and such;
+  // wonder if theres anything similar for java?
